dbc8ff2629912dc959d710e56367f7a958391992
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-14 15:43:33 -0600

    Finished section 2

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// I. Variables & Datatypes
+// A. Q + A
+// How do we assign a value to a variable?
+// How do we change the value of a variable?
+// How do we assign an existing variable to a new variable?
+// Remind me, what are declare, assign, and define?
+// What is pseudocoding and why should you do it?
+// What percentage of time should be spent thinking about how 
+// you're going to solve a problem vs actually typing in code 
+// to solve it?
+// ANSWERS
+// -by using = and then the value
+// -assign it a new value within a scope
+// -set them equal to each other
+// -Declare is to establish a new variable
+// -assign is to give an existing variable a value,
+// -define is to give a variable a place
+// -pseudocoding is writing out logical steps to use for coding problems
+// -70-80%
+// B. Strings
+// Create a variable called firstVariable.
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable?
+// Create a variable called yourName and set it equal to your name as a string. 
+// Then, write an expression that takes the string "Hello, my name 
+// is " and the variable yourName so that 
+// it returns a new string with them concatenated.
+// ex: Hello, my name is Jean Valjean
+
+let firstVariable = "Hello World";
+firstVariable = 1;
+let secondVariable = firstVariable;
+secondVariable = "Hello !World";
+console.log(firstVariable);
+1
+const yourName = "Donnie"
+console.log("Hello, my name is " + yourName);
