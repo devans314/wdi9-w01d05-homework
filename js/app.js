8873debbf2b8962b7174dd4c019d5422072e963d
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-11-15 19:35:28 -0700

    actually finished loops

--- js/app.js
+++ js/app.js
@@ -135,14 +135,26 @@
 // Check your work! Your banck_account should have $55 in it.
 
 // You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
-let bankAccount = 0;
-for(let i = 1; i <= 10; i++){
-    bankAccount+= i;
-}
-for(let i = 1; i <= 100; i++){
-    bankAccount+= 2*i;
-} 
-console.log(bankAccount);
+// let bankAccount = 0;
+// for(let i = 1; i <= 10; i++){
+//     bankAccount+= i;
+// }
+// for(let i = 1; i <= 100; i++){
+//     bankAccount+= 2*i;
+// } 
+// console.log(bankAccount);
+
+// E. Multiples of 3 and 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000. If a previous question you've done has helpful bits of code in it that partially solves this problem, look back at them.
+
+// You just solved Project Euler problem 1!
+
+// Are you having dejà vu? This just in! From the "Read the entire problem before you start" dept: This problem was on a previous assignment. You may skip it if you've already done it, just include a comment saying that you've already done it. If you've now done the problem twice, perhaps next time you'll read the whole problem before starting it.
+
+
+// I completed this earlier (as in literally tonight).
 
 //######FUNCTIONS######
 // ### I. `transmogrify`
