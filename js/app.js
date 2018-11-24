df4f19041313a9d2984c3f684aff5cfdfca1d653
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-11-24 16:27:45 -0700

    finished F under objects

--- js/app.js
+++ js/app.js
@@ -579,13 +579,74 @@ const user = {
     purchased: [],
 };
 
+// B. Update the user
+// Our user has changed his or her email address. Without changing the original user object, update the email value to a new email address.
+// Our user has had a birthday! Without changing the original user object, increment the age value using the postfix operator. Hint: age++
+
 user.email = 'that.nithe.com'
 user.age++
+
+// C. Adding keys and values
+// You have decided to add your user's location to the data that you want to collect.
+
+// Without changing the original user object, add a new key location to the object, and give it a value or some-or-other location (a string).
+
 user.location = 'Colorado'
+
+// D. Shopaholic!
+// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchased array.
+// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchased array.
+// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchased array.
+// Console.log just the "Merino jodhpurs" from the purchased array.
+
 user.purchased.push('Carbohydrates');
 user.purchased.push('peace of mind');
 user.purchased.push('Merino jodhpers');
 console.log(user.purchased[2]);
+
+// E. Object-within-object
+// Remember that you can add an object to an existing object in the same way that you can add any new property/value pair.
+
+// If we want to give our user a friend with a name and age, we could write:
+
+// user.friend = {
+//     name: "Grace Hopper",
+//     age: 85
+// }
+// When we console.log user, we would see the friend object added to our user object.
+
+// Write a friend object into your user object and give the friend a name, age, location, and purchased array (empty for now)
+// Console.log just the friend's name
+// Console.log just the friend's location
+// CHANGE the friend's age to 55
+// The friend has purchased "The One Ring". Use .push() to add "The One Ring" to the friend's purchased array.
+// The friend has purchased "A latte". Use .push() to add "A latte" to the friend's purchased array.
+// Console.log just "A latte" from the friend's purchased array.
+
+// user.friend = {
+//     name: 'Adam Sandler',
+//     email: 'billy.madison.gmail.com',
+//     age: 49,
+//     purchased: [],
+//     location: 'netflix',
+// };
+// console.log(user.friend.name);
+// console.log(user.friend.location);
+// user.friend.age = 55;
+// user.friend.purchased.push('The One Ring');
+// user.friend.purchased.push('A Latte');
+// console.log(user.friend.purchased[1]);
+
+// F. Loops
+// Write a for loop that iterates over the User's purchased array (NOT the friend's purchased array), and prints each element to the console.
+// Write a for loop that iterates over the Friend's purchased array, and prints each element to the console.
+
+// for ( let i = 0; i < user.purchased.length; i++){
+//     console.log(user.purchased[i]);
+// };
+// for ( let i = 0; i < user.friend.purchased.length; i++){
+//     console.log(user.friend.purchased[i]);
+// };
 
 
 
