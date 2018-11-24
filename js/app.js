6eef71e3ac7e419b2b013ac0ee647f271b25f56f
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-11-24 14:42:18 -0700

    finished C under arrays

--- js/app.js
+++ js/app.js
@@ -178,6 +178,26 @@
 //     "One good thing about music is that when it hits you, you feel no pain"
 // ]
 
+// C. Accessing elements
+// Given the following array const randomThings = [1, 10, "Hello", true]
+
+// How do you access the 1st element in the array?
+// randomThings[0];
+// Change the value of "Hello" to "World".
+// randomThings[2] = "world";
+// console.log(randomThings[2]);
+
+// Check the value of the array to make sure it updated the array. How? Why, yes! console.log();
+
+// D. Change values
+// Given the following array const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+
+// What would you write to access the 3rd element of the array?
+
+// Change the value of "Github" to "Octocat"
+
+// Add a new element, "Cloud City" to the array.
+
 //######FUNCTIONS######
 // ### I. `transmogrify`
 
