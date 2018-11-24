bea42c96cefc310fbff3889de295e458e5e08873
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-11-24 15:07:09 -0700

    finished G under arrays

--- js/app.js
+++ js/app.js
@@ -243,6 +243,24 @@
 //     console.log('little number')
 // } else {console.log('big number')}
 
+// G. Monkey in the Middle
+// Write an if ... else if ... else statement:
+
+// console.log() little number if the number entered is less than 5.
+
+// If the number entered is more than 10, log big number.
+
+// Otherwise, log "monkey". 
+
+// if(integer < 5){
+//     console.log('little number');
+// } else if(integer > 10){
+//     console.log('big number');
+// } else{
+//     console.log('monkey');
+// }
+
+
 //######FUNCTIONS######
 // ### I. `transmogrify`
 
